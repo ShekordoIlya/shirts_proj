713c03dd22fc5ffca44e6037fee3b5dc8730c814
Шекордо Илья Сергеевич <shekordo_is@peleng.jsc.local>
2026-02-25 15:55:15 +0300

    fix(app): fixed unusual files

---
+++ shirts_proj/src/Components/MainSection/MainSection.jsx
@@ -0,0 +1,7 @@
+import styles from "./MainSection.module.scss";
+
+const MainSection = () => {
+  return <div className={styles.mainSec}></div>;
+};
+
+export default MainSection;
--- shirts_proj/src/main.jsx
+++ shirts_proj/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MainSection from "./Components/MainSection/MainSection";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MainSection />
   </StrictMode>,
-)
+);
